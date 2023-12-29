4c2642c261c6d1025969a4a2ec2353537443cc46
Callum Baldwin <developer@ladish.dev>
2023-12-29 20:30:25 +0000

    Add routes for viewing data and error handling

--- electoral-system.py
+++ electoral-system.py
@@ -4,15 +4,33 @@ import os
 
 app = Flask(__name__, static_url_path='/static')
 
-#gamedb = mysql.connector.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASS'),
-#                              host='192.168.0.23', database='electoral_system',
-#                              auth_plugin='mysql_native_password')
+electoraldb = mysql.connector.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASS'),
+                              host='localhost', database='electoralsystem',
+                              auth_plugin='mysql_native_password')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if "viewdata" in request.form:
+        return render_template('viewdata.html')
+    else:
+        return render_template('errorpage.html')
 
+@app.route('/viewdata', methods=['GET', 'POST'])
+def viewdata():
+    if "viewalldata" in request.form:
+        return render_template('test.html')
+    elif "back" in request.form:
+        return render_template('index.html')
+    else:
+        return render_template('errorpage.html')
+
+@app.route('/errorpage', methods=['GET', 'POST'])
+def errorpage():
+    if "back" in request.form:
+        return render_template('index.html')
+    else:
+        return render_template('errorpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

67fbd9e933daecb3c209bafe5ffa986ea7f30660
Callum Baldwin <developer@ladish.dev>
2024-01-12 19:38:17 +0000

    Complete overhall
    Reduced the amount of pages to make it more simple as was doing same thing in many files.
    
    Made it so the back button will return back to the page u were just at even when the data are using all the same page.

--- electoral-system.py
+++ electoral-system.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 import os
 import atexit
@@ -56,7 +56,7 @@ def viewdata():
     if "viewalldata" in request.form:
         return render_template('view_all_data.html', data=view_all_data())
     elif "fptpseats" in request.form:
-        return render_template('fptp_seats_data.html', data=calculate_fptp_seats())
+        return render_template('sort_data.html', data=fptp_seats())
     elif "sprelection" in request.form:  
         return render_template('spr_election.html')
     elif "lrelection" in request.form:
@@ -64,17 +64,10 @@ def viewdata():
     elif "dhondt" in request.form:
         return render_template('dhondt.html')
     else:
-        return render_template('errorpage.html')
+        return render_template('index.html')
     
 @app.route('/viewalldata', methods=['GET', 'POST'])
 def viewalldata():
-    if "back" in request.form:
-        return render_template('index.html')
-    else:
-        return render_template('errorpage.html')
-    
-@app.route('/fptpdata', methods=['GET', 'POST'])
-def fptpdata():
     if "back" in request.form:
         return render_template('index.html')
     else:
@@ -83,44 +76,46 @@ def fptpdata():
 @app.route('/sprelection', methods=['GET', 'POST'])
 def sprelection():
     if "electionspr" in request.form:
-        return render_template('spr_election_data.html', data=calculate_election_spr("All Seats"))
+        return render_template('sort_data.html', data=election_spr("All Seats"))
     elif "electionsprthreshold" in request.form:
-        return render_template('spr_election_data.html', data=calculate_election_spr("All Seats", 5))
+        return render_template('sort_data.html', data=election_spr("All Seats", 5))
     elif "electionsprcounty" in request.form:
-        return render_template('sort_by_data.html', data=calculate_election_spr("County"))
+        return render_template('sort_by_data.html', data=election_spr("County"))
     elif "electionsprregion" in request.form:
-        return render_template('sort_by_data.html', data=calculate_election_spr("Region"))
+        return render_template('sort_by_data.html', data=election_spr("Region"))
     elif "electionsprcountry" in request.form:
-        return render_template('sort_by_data.html', data=calculate_election_spr("Country"))
+        return render_template('sort_by_data.html', data=election_spr("Country"))
     elif "back" in request.form:
         return render_template('index.html')
     else:
-        return render_template('errorpage.html')
-
-@app.route('/sprelectiondata', methods=['GET', 'POST'])
-def sprelectiondata():
-    if "back" in request.form:
         return render_template('spr_election.html')
-    else:
-        return render_template('errorpage.html')
     
 @app.route('/lrelection', methods=['GET', 'POST'])
 def lrelection():
     if "lrelectioncounty" in request.form:
-        return render_template('lr_election_data.html', data=calculate_election_lr("County"))
+        return render_template('sort_by_data.html', data=election_lr("County"))
     elif "lrelectionregion" in request.form:
-        return render_template('lr_election_data.html', data=calculate_election_lr("Region"))
+        return render_template('sort_by_data.html', data=election_lr("Region"))
     elif "lrelectioncountry" in request.form:
-        return render_template('lr_election_data.html', data=calculate_election_lr("Country"))
+        return render_template('sort_by_data.html', data=election_lr("Country"))
     elif "back" in request.form:
         return render_template('index.html')
     else:
+        return render_template('lr_election.html')
+    
+@app.route('/sortdatapage', methods=['GET', 'POST'])
+def sprelectiondata():
+    if "back" in request.form:
+        print("Referrer:", request.referrer)
+        return redirect(request.referrer)
+    else:
         return render_template('errorpage.html')
     
-@app.route('/lrelectiondata', methods=['GET', 'POST'])
+@app.route('/sortbydatapage', methods=['GET', 'POST'])
 def lrelectiondata():
     if "back" in request.form:
-        return render_template('lr_election.html')
+        print("Referrer:", request.referrer)
+        return redirect(request.referrer)
     else:
         return render_template('errorpage.html')
     
@@ -167,7 +162,11 @@ def view_all_data():
     data = cur.fetchall()
     return data
 
-def calculate_fptp_seats(level=None, threshold=None):
+def fptp_seats():
+    # dictionary to store the data
+    page_info = {}
+    page_info['page_title'] = "First Past The Post Data"
+    page_info['page_description'] = "First Past The Post"
     cur = electoraldb.cursor()
 
     # SQL query to get winning constituency in each region and sum their votes
@@ -194,15 +193,14 @@ def calculate_fptp_seats(level=None, threshold=None):
     data_dict = {row[0]: {'votes': row[1], 'seats': row[2], 'percentage_seats': row[3], 'percentage_votes': row[4], 'difference_in_seats_votes': row[5], 'difference_from_winner': row[6]} for row in data}
 
     # Sort the dictionary by the number of seats won
-    sorted_data = dict(sorted(data_dict.items(), key=lambda item: item[1]['seats'], reverse=True))
-    return sorted_data
+    data_dict = dict(sorted(data_dict.items(), key=lambda item: item[1]['seats'], reverse=True))
+    return page_info, data_dict
 
-
-def calculate_election_spr(level=None, threshold=None):
+def election_spr(level=None, threshold=None):
     operation_name = f"{level} - Threshold {threshold}%" if threshold is not None else level
     # dictionary to store the data
     page_info = {}
-    page_info['page_title'] = "Election Results"
+    page_info['page_title'] = "Proportional Representation Data"
     page_info['page_description'] = operation_name
     cur = electoraldb.cursor()
 
@@ -266,84 +264,51 @@ def calculate_election_spr(level=None, threshold=None):
 
 
 # General Election seats allocations based on Largest Remainder ( County, Region, Country )
-def calculate_election_lr(level=None):
-    # Get data from the database
-    cur = electoraldb.cursor(dictionary=True)
+def election_lr(level=None):
+    operation_name = f"{level}" if level is not None else level
+    # dictionary to store the data
+    page_info = {}
+    page_info['page_title'] = "Largest Remainder Data"
+    page_info['page_description'] = operation_name
 
-    # Determine the column names and join table based on the specified level
-    if level == "County":
-        column_name = "c.countyName"
-        join_table = "county c"
-        join_condition = "con.countyID = c.countyID"
-    elif level == "Region":
-        column_name = "r.regionName"
-        join_table = "region r"
-        join_condition = "con.regionID = r.regionID"
-    elif level == "Country":
-        column_name = "co.countryName"
-        join_table = "country co"
-        join_condition = "con.countryID = co.countryID"
-    else:
-        raise ValueError("Invalid level specified. Please choose from: 'County', 'Region', 'Country'")
-
-    # SQL query to get all the votes for each party by the specified level
+    # Get data from the database based on the level
+    cur = electoraldb.cursor()
     cur.execute(f'''
-        SELECT {column_name} AS geo_name, p.partyName AS party, SUM(cd.votes) AS total_votes
-        FROM candidate cd
-        JOIN party p ON cd.partyID = p.partyID
-        JOIN constituency con ON cd.constituencyID = con.constituencyID
-        JOIN {join_table} ON {join_condition}
-        GROUP BY geo_name, party;
-    ''')
-
+            SELECT 
+                systemName AS `System`,
+                partyName AS Party,
+                votes AS Votes, 
+                seats AS 'Seats Won', 
+                percentage_seats AS 'Percentage Of Seats',
+                percentage_votes AS 'Percentage of Votes', 
+                difference_in_seats_votes AS 'Difference in Percentages'
+            FROM
+                electionresults
+            WHERE
+                systemName LIKE 'Largest Remainder - {level}%'
+            ''')
+    
     # Fetch the results
-    pr_results = cur.fetchall()
+    data = cur.fetchall()
+
+    # Convert the data to a dictionary
+    data_dict = {}
+    for row in data:
+        system_parts = row[0].strip().split(" - ")
+        geo_name = system_parts[2]  # get the part of the systemName after the second -
+        party = row[1]
+        if geo_name not in data_dict:
+            data_dict[geo_name] = {}
+        data_dict[geo_name][party] = {'votes': row[2], 'seats': row[3], 'percentage_seats': row[4], 'percentage_votes': row[5], 'difference_in_seats_votes': row[6]}
+
+    # Sort by the number of seats won within each geo_name
+    for geo_name in data_dict:
+        data_dict[geo_name] = dict(sorted(data_dict[geo_name].items(), key=lambda item: item[1]['seats'], reverse=True))
+
     cur.close()
+    return page_info, data_dict
 
-    # Calculate the sum of total votes
-    total_votes_sum = sum(float(result['total_votes']) for result in pr_results)
 
-    # Calculate the Hare Quota
-    hare_quota = total_votes_sum / 650
-
-    # allocated seats = total votes per party / Hare Quota and store remainder in a dictionary
-    allocated_seats = {}
-    for result in pr_results:
-        geo_name = result['geo_name']
-        party = result['party']
-        total_votes = float(result['total_votes'])
-        if geo_name not in allocated_seats:
-            allocated_seats[geo_name] = {}
-        allocated_seats[geo_name][party] = {
-            'votes': total_votes,
-            'seats': int(total_votes / hare_quota),
-            'remainder': total_votes % hare_quota
-        }
-
-    # Sort the allocated seats by the remainder in descending order
-    for geo_name in allocated_seats:
-        allocated_seats[geo_name] = dict(sorted(allocated_seats[geo_name].items(), key=lambda item: item[1]['remainder'], reverse=True))
-
-    # remaining seats are allocated to parties with the highest remainders
-    # and if you reach the bottom with seats left, allocate the seats at the top
-    remaining_seats = 650 - sum(sum(allocated_seats[geo_name][party]['seats'] for party in allocated_seats[geo_name]) for geo_name in allocated_seats)
-    allocated_remainder = {geo_name: {party: allocated_seats[geo_name][party]['seats'] for party in allocated_seats[geo_name]} for geo_name in allocated_seats}
-
-    for geo_name in allocated_seats:
-        for party in sorted(allocated_seats[geo_name].keys(), key=lambda x: allocated_seats[geo_name][x]['remainder'], reverse=True)[:remaining_seats]:
-            allocated_remainder[geo_name][party] += 1
-
-    data = {geo_name: {party: {
-                    'votes': int(allocated_seats[geo_name][party]['votes']),
-                    'seats': allocated_seats[geo_name][party]['seats'] + (allocated_remainder[geo_name][party] - allocated_seats[geo_name][party]['seats']),
-                    'percentage_seats': f"{((allocated_seats[geo_name][party]['seats'] + (allocated_remainder[geo_name][party] - allocated_seats[geo_name][party]['seats'])) / 650) * 100:.2f}%",
-                    'percentage_votes': f"{(allocated_seats[geo_name][party]['votes'] / total_votes_sum) * 100:.2f}%",
-                    'difference_in_seats_votes': f"{((allocated_seats[geo_name][party]['seats'] + (allocated_remainder[geo_name][party] - allocated_seats[geo_name][party]['seats'])) / 650) * 100 - (allocated_seats[geo_name][party]['votes'] / total_votes_sum) * 100:.2f}%",
-                } 
-                for party in allocated_seats[geo_name]}
-            for geo_name in allocated_seats}
-
-    return level, data
 
 
 # General Election seats allocations based on D'Hondt method ( County, Region, Country )
